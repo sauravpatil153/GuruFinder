f29b57d67c313d56fef07a259915d740c6fb994a
Saurav Patil <sauravpatil153@gmail.com>
2023-02-24 17:02:37 +0530

    pojos mapping updated

--- GuruFinder/src/main/java/com/app/pojos/Tutor.java
+++ GuruFinder/src/main/java/com/app/pojos/Tutor.java
@@ -10,56 +10,82 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
 
 @Entity
 @Table(name = "tutors")
+@Audited
 public class Tutor {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "tutor_id")
 	private Long tutorId;
+	@NotAudited
 	@Column(name = "first_name", length = 30, nullable = false)
 	private String firstName;
+	@NotAudited
 	@Column(name = "last_name", length = 30, nullable = false)
 	private String lastName;
+	@NotAudited
 	@Column(name = "mobile_no", length = 12, nullable = false)
 	private String mobileNo;
+	@NotAudited
 	@Column(length = 10, nullable = false)
 	private String gender;
+	@NotAudited
 	@Column(name = "email_id", length = 50, nullable = false, unique = true)
 	private String emailId;
+	@NotAudited
 	@Column(length = 20, nullable = false)
 	private String password;
+	@NotAudited
 	@Lob
 	@Column(name = "profile_photo", nullable = false)
 	private byte[] profilePhoto;
+	@NotAudited
 	@Lob
 	@Column(name = "id_proof", nullable = false)
 	private byte[] idProof;
+	@NotAudited
 	@Column(nullable = false)
 	private LocalDate dob;
+	@NotAudited
 	@Column(length = 150, nullable = false)
 	private String tagline;
+	@NotAudited
 	@Column(nullable = false)
 	private String summary;
+	@NotAudited
 	@Column(name = "total_experience", nullable = false)
 	private Integer totalExperience;
-	
+	@CreationTimestamp
+	@JoinColumn(name = "registration_date")
+	private LocalDate registrationDate;
+	private String status;
+	@NotAudited
 	@OneToOne(mappedBy = "tutor", cascade = CascadeType.ALL, orphanRemoval = true)
 	private Address address;
 	
+	@NotAudited
 	@OneToMany(mappedBy = "tutor", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<TutorQualifications> qualifications=new ArrayList<TutorQualifications>();
 	
+	@NotAudited
 	@OneToMany(mappedBy = "tutor", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<TutorExperience> experiences=new ArrayList<TutorExperience>();
 	
+	@NotAudited
 	@OneToMany(mappedBy = "tutor", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<CourseBrochure> courseBrochers=new ArrayList<CourseBrochure>();
+	private List<CourseBrochure> courseBrochures =new ArrayList<CourseBrochure>();
 	
 	public Tutor() {
 		super();
